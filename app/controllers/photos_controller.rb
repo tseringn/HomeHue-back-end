0b8e48fdf8e5e0f9236066c9a4984eda1c9ce378
callas4ever <thetseringnorbu@gmail.com>
2020-07-24 08:53:50 -0400

    figured out api

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,16 @@
-require 'rubygems' if RUBY_VERSION < '1.9'
-require 'rest-client'
-require 'base64'
+
 
 class PhotosController < ApplicationController
 
     def create
-        image_url = 'https://i.ytimg.com/vi/dxAutV5azRU/hqdefault.jpg'
-        api_key = 'acc_81ff1e078ba035c'
-        api_secret = '16b40a226339a96959101666022620f0'
-
-        auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
-        response = RestClient.get "https://api.imagga.com/v2/colors?image_url=#{image_url}", { :Authorization => auth }
-        puts response
+            image_url = 'https://i.ytimg.com/vi/dxAutV5azRU/hqdefault.jpg'
+            api_key = 'acc_ffc1bc4fd1a6a9c'
+            api_secret = 'ee61eb72c1179f1b494b78ae6672a571'
+      
+            auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
+            response = RestClient.get "https://api.imagga.com/v2/colors?image_url=#{image_url}", { :Authorization => auth }
+            puts response
+     end
 
         # Photo.create(img_url: photo_params[img_url], room_id: photo_params[room_id], color1 )
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,20 +1,7 @@
 
 
-require 'net/http'
-require 'open-uri'
-require 'json'
-
 class Photo < ApplicationRecord
   belongs_to :room
-  def self.get_api
-      image_url = 'https://i.ytimg.com/vi/dxAutV5azRU/hqdefault.jpg'
-      
-      api_key = 'acc_ffc1bc4fd1a6a9c'
-      api_secret = 'ee61eb72c1179f1b494b78ae6672a571'
-
-      auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
-      response = RestClient.get "https://api.imagga.com/v2/colors?image_url=#{image_url}", { :Authorization => auth }
-      puts response
-  end
+  
 
 end
