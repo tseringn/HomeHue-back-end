cd1996ca88e93bbc677b7434f42b259d2d728197
callas4ever <thetseringnorbu@gmail.com>
2020-07-24 16:21:20 -0400

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,31 @@ class UsersController < ApplicationController
     def index
         users=User.all
         render json: users
-        end
+    end
 
     def new
     
     end
-
+    def create
+    user=User.create(user_params)
+    render json: user
+    end
     def destroy
     
     end
 
     def show
-    
+        user=User.find(params[:id])
+        render json: user
     end
 
     def update
     
     end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :username, :email, :password, :image_url)
+    end
+
 end
