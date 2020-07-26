d1fd7a6e5d01f4ebb4570a3a077c5c378ed6f743
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-26 17:49:19 -0400

    rendering user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
     def index
         users=User.all
-        render json: users, include:[:rooms, :likes]
+
+        render :json => users.to_json(:include => {:rooms => {:include=> :likes}})
+
     end
 
     def new
     
     end
+
     def create
     user=User.create(user_params)
     render json: user
     end
+
     def destroy
     
     end
