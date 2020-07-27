806ceb9475ae8bd41cc9edfd33c2e2ce08e51c50
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-27 12:16:31 -0400

    changed private collumn on rooms to pvt bc private is saved word, create action in rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,15 @@ class RoomsController < ApplicationController
     render json: rooms, include:[:likes, :user]
     end
 
-    
+    def create
+        room = Room.create(room_params)
+        render json: room, include:[:likes, :user]
+    end
+
+    private
+
+    def room_params
+        params.require(:room).permit(:user_id, :name, :description, :img_url, :pvt)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_211532) do
+ActiveRecord::Schema.define(version: 2020_07_27_142319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_211532) do
     t.string "name"
     t.string "description"
     t.string "img_url"
-    t.boolean "private"
+    t.boolean "pvt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_rooms_on_user_id"
