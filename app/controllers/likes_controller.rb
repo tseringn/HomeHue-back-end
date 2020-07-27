491a3829d2f8d468e773b3b2a7ec02d012fb9d8f
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-27 17:06:21 -0400

    dd

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+
+    def create
+        like=Like.create(like_params)
+        render json: like
+    end
+
+    def destroy
+        like=Like.find(params[:id])
+        like.destroy
+        render json: "like deleted"
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :room_id)
+    end
 end
