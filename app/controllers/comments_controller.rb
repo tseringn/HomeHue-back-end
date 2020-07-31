757752daa6589caaccd521d4d338ffdcf4421726
callas4ever <thetseringnorbu@gmail.com>
2020-07-31 12:03:51 -0400

    added update, destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
+
+    def index
+    comments=Comment.all
+    render json: comments
+    end
+
     def create
         comment = Comment.create(comment_params)
         render json: comment, include: :user
+    end
+
+    def update
+    comment=Comment.find(params[:id])
+    comment.update(comment_params)
+    render json: comment
+    end
+
+    def destroy
+    comment=Comment.find(params[:id])
+    comment.destroy
+    render json: 'deleted!'
     end
 
     private
